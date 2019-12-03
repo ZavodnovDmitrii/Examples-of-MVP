a2de8e0c00f567a0834ab0910053b9cb98548954
Dmitrii Zavodnov <dmitrii.zavodnov@mail.ru>
2019-12-03 12:19:39 +0300

    create tableView

--- MVP_part 1/MainModule/View/MainViewController.swift
+++ MVP_part 1/MainModule/View/MainViewController.swift
@@ -3,21 +3,33 @@ import UIKit
 
 class MainViewController: UIViewController {
     
-    @IBOutlet weak var greetingLabel: UILabel!
+    @IBOutlet weak var tableView: UITableView!
     
     var presenter: MainViewPresenterProtocol!
-
+    
     override func viewDidLoad() {
         super.viewDidLoad()
+        
+        tableView.register(UITableViewCell.self, forCellReuseIdentifier: "Cell")
+        tableView.dataSource = self
+        tableView.delegate = self
+    }
+}
+
+extension MainViewController: UITableViewDataSource, UITableViewDelegate {
+    func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        return 2
     }
     
-    @IBAction func didTapButton(_ sender: Any) {
-        self.presenter.showGreeting()
+    func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        let cell = tableView.dequeueReusableCell(withIdentifier: "Cell", for: indexPath)
+        cell.textLabel?.text = "Test"
+        return cell
     }
 }
 
 extension MainViewController: MainViewProtocol {
     func setGreeting(greeting: String) {
-        self.greetingLabel.text = greeting
     }
 }
+
